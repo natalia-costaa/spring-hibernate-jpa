ac3490904e70a6e9b5f371081bed4727e35893b1
natalia-costaa <ncalbuq@gmail.com>
2023-11-07 14:24:05 -0300

    Update CruddemoApplication.java
    criação do metodo createStudent e adicionado no metodo CommandLinner

--- src/main/java/com/luv2/code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2/code/cruddemo/CruddemoApplication.java
@@ -1,5 +1,7 @@
 package com.luv2.code.cruddemo;
 
+import com.luv2.code.cruddemo.dao.StudentDAO;
+import com.luv2.code.cruddemo.entity.Student;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,10 +15,25 @@ public class CruddemoApplication {
 	}
 
 	@Bean
-	public CommandLineRunner commandLineRunner(String[] args) {
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			System.out.println("Hello World");
+			createStudent(studentDAO);
 		};
+
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+
+		// create the student object
+		System.out.println("Creating new Student Object...");
+		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+
+		// save the student object
+		System.out.println("Savinf the Student...");
+		studentDAO.save(tempStudent);
+
+		// display id of the saved student
+		System.out.println("Saved student. Generated id: " + tempStudent.getId());
 
 	}
 
