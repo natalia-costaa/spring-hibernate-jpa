c461baac3232550bbe4ecb3280135c9227933657
natalia-costaa <ncalbuq@gmail.com>
2023-11-07 14:44:49 -0300

    Update CruddemoApplication.java
    criação de método createMultipleStudents()

--- src/main/java/com/luv2/code/cruddemo/CruddemoApplication.java
+++ src/main/java/com/luv2/code/cruddemo/CruddemoApplication.java
@@ -17,23 +17,45 @@ public class CruddemoApplication {
 	@Bean
 	public CommandLineRunner commandLineRunner(StudentDAO studentDAO) {
 		return runner -> {
-			createStudent(studentDAO);
+			createMultipleStudent(studentDAO);
 		};
 
 	}
 
-	private void createStudent(StudentDAO studentDAO) {
+	private void createMultipleStudent(StudentDAO studentDAO) {
 
-		// create the student object
-		System.out.println("Creating new Student Object...");
-		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+		// create multiple students
+		System.out.println("Creating 3 Student Object...");
+		Student tempStudent1 = new Student("John", "Doe", "paul@luv2code.com");
+		Student tempStudent2 = new Student("Mary", "Public", "mary@luv2code.com");
+		Student tempStudent3 = new Student("Bonita", "Applebum", "bonita@luv2code.com");
 
 		// save the student object
-		System.out.println("Savinf the Student...");
-		studentDAO.save(tempStudent);
+		System.out.println("Saving the Student...");
+		studentDAO.save(tempStudent1);
+		studentDAO.save(tempStudent2);
+		studentDAO.save(tempStudent3);
 
 		// display id of the saved student
-		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+		System.out.println("Saved student. Generated id: " + tempStudent1.getId());
+		System.out.println("Saved student. Generated id: " + tempStudent2.getId());
+		System.out.println("Saved student. Generated id: " + tempStudent3.getId());
+
+//	private void createStudent(StudentDAO studentDAO) {
+//
+//		// create the student object
+//		System.out.println("Creating new Student Object...");
+//		Student tempStudent = new Student("Paul", "Doe", "paul@luv2code.com");
+//
+//		// save the student object
+//		System.out.println("Savinf the Student...");
+//		studentDAO.save(tempStudent);
+//
+//		// display id of the saved student
+//		System.out.println("Saved student. Generated id: " + tempStudent.getId());
+//
+//	}
+
 
 	}
 
