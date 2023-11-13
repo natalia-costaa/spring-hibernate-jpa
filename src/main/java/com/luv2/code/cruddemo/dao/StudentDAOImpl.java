6fb58b1dfe823972ced55cb905c12dbe34f51c63
natalia-costaa <ncalbuq@gmail.com>
2023-11-13 14:26:25 -0300

    Update StudentDAOImpl.java

--- src/main/java/com/luv2/code/cruddemo/dao/StudentDAOImpl.java
+++ src/main/java/com/luv2/code/cruddemo/dao/StudentDAOImpl.java
@@ -42,4 +42,16 @@ public class StudentDAOImpl implements StudentDAO{
         // return query results
         return theQuery.getResultList();
     }
+
+    @Override
+    public List<Student> findByLastName(String theLastName) {
+        // create query
+        TypedQuery<Student> theQuery = entityManager.createQuery(
+                "FROM Student WHERE lastName=:theData ",Student.class);
+
+        theQuery.setParameter("theData",theLastName);
+
+        // return query results
+        return theQuery.getResultList();
+    }
 }
