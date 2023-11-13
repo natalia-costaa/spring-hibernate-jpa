31f132868a44548391401bf1ae5cddbdac753cf8
natalia-costaa <ncalbuq@gmail.com>
2023-11-13 11:36:19 -0300

    Update StudentDAO.java
    adição método findall()

--- src/main/java/com/luv2/code/cruddemo/dao/StudentDAO.java
+++ src/main/java/com/luv2/code/cruddemo/dao/StudentDAO.java
@@ -2,11 +2,15 @@ package com.luv2.code.cruddemo.dao;
 
 import com.luv2.code.cruddemo.entity.Student;
 
+import java.util.List;
+
 public interface StudentDAO {
 
     void save(Student theStudent);
 
     Student findById(Integer id);
+
+    List<Student> findAll();
 
 
 
